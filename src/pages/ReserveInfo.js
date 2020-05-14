0c1dd039cf821a83c5ff78658ed7f564a89c8029
kai <zk.zhengkai@outlook.com>
2020-05-13 20:46:01 -0400

    basic set up

---
+++ src/pages/ReserveInfo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ReserveInfo = () => {
+    return (
+        <div>
+            reserve info page
+        </div>
+    )
+}
+export default ReserveInfo;
---
+++ src/pages/Rooms.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Rooms = () => {
+    return (
+        <div>
+            rooms page
+        </div>
+    )
+}
+export default Rooms;
