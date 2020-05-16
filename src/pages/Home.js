e1116ad68f780390dcf78317bb9a23c562fbc33b
Qianfan <guoqianfan2013@gmail.com>
2020-05-15 21:39:37 -0400

    a

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Hero from '../components/Hero'
 export default function Home() {
     return (
-        <div>
-            home page
-        </div>
+        <Hero>
+
+        </Hero>
     )
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import Hero from '../components/Hero'
 const Login = () => {
     return (
+        <>
+        
         <div>
             Login
         </div>
+        <Hero></Hero>
+        </>
+        
     )
 }
 export default Login;
