c4eea31519f452d9d1776c3233ecccaf5f984445
kai <zk.zhengkai@outlook.com>
2020-05-17 17:11:25 -0400

    add Banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import Hero from "../components/Hero";
+import Hero from "../components/Hero"
+import Banner from "../components/Banner"
+import {Link} from 'react-router-dom'
 import Services from "../components/Services"
 
 export default function Home() {
     return (
         <>
-        <Hero></Hero>
+        <Hero>
+            <Banner title="Resting Space" subtitle="Room for You to Rest">
+                <Link to="rooms" className="btn-primary">
+                    Rooms
+                </Link>
+            </Banner>
+        </Hero>
         <Services></Services>
         </>
     )
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,7 +1,15 @@
 import React from 'react'
-import Hero from "../components/Hero";
+import Hero from "../components/Hero"
+import Banner from "../components/Banner"
+import {Link} from 'react-router-dom'
 
 const Rooms = () => {
-    return <Hero hero="roomsHero"></Hero>;
+    return <Hero hero="roomsHero">
+        <Banner title="rooms">
+            <Link to='/' className="btn-primary">
+                return home
+            </Link>
+        </Banner>
+    </Hero>;
 }
 export default Rooms;
