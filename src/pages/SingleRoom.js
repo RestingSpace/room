a75366ea78753bad129ee87890781f734e262d6e
jiuchao <jiuchao1@gmail.com>
2020-05-25 21:56:35 -0400

    Add StyledHero

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,64 +1,97 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
+import {RoomContext} from "../context"
 import defaultBcg from '../images/meditation-room.jpg';
 import Calendar from "../components/Calendar";
 import Navbar from "../components/Navbar"
-//import Hero from '../components/Hero';
-//import Banner from '../Components/Banner';
-//import {Link} from 'react-router-dom';
-//import {RoomContext} from '../context';
+import StyledHero from "../components/StyledHero"
+
 export default class SingleRoom extends Component {
     //Access the URL parameter
     constructor(props) {
         super(props);
-        this.state={
-            //slug:this.props.match.params.slug,
+        //console.log(this.props);
+        this.state = {
+            slug: this.props.match.params.slug,
             defaultBcg: defaultBcg
         };
     }
 
-    //static  contextType = RoomContext;
+    static  contextType = RoomContext;
+
     //componentDidMount() {}
 
     render() {
-        //const {getRoom} = this,context;
-        //const room = getRoom(this.state.slug);
+        const {getRoom} = this.context;
+        const room = getRoom(this.state.slug);
+        if (!room) {
+            return <div className="error">
+                <h3>no such room could be found...</h3>
+                <Link to='/rooms' className="btn-primary">
+                    back to rooms
+                </Link>
+            </div>
+        }
+        ;
 
+        const {name, description, capacity, size, price, extras, pets, images} = room;
+        const [mainImg, ...defaultImg] = images;
         return (
             <>
-                <Navbar />
-                <section className="single-room">
-                    <div className = "single-room-images"><img src={defaultBcg} alt="Room"></img></div>
+                <Navbar/>
 
+                <StyledHero img={mainImg || this.state.defaultBcg}>
+                    <Banner title={`${name} room`}>
+                        <Link to='/rooms' className='btn-primary'>
+                            back to rooms
+                        </Link>
+                    </Banner>
+                </StyledHero>
+                <section className="single-room">
+                    <div className="single-room-images">
+                        {defaultImg.map((item,index)=>{
+                           return <img key={index} src={item} alt={name}/>
+                        })}
+                    </div>
 
                     <div className="single-room-info">
                         <article className="desc">
                             <h3>details</h3>
-                            <p>Meditation Room</p>
+                            <p>{description}</p>
                         </article>
                         <article className="info">
                             <h3>info</h3>
-                            <h6>price : $10 </h6>
-                            <h6>size : 10 SQFT</h6>
+                            <h6>price : ${price} </h6>
+                            <h6>size : {size} SQFT</h6>
                             <h6>
-                                max capacity : 4
+                                max capacity : {" "}
+                                {
+                                capacity > 1? `${capacity} people`:`${capacity} person`
+                                }
                             </h6>
-                            <h6>Pet: no pets allowed</h6>
-                            <h6>Bed: not available</h6>
-                            <h6>Bath Room: not available</h6>
+                            <h6>{pets?"pets allowed": "no pets allowed"}</h6>
                             <h6>wifi: yes </h6>
-
                         </article>
                     </div>
                 </section>
+
                 <section className="room-extras">
+                    <h6>extras</h6>
+                    <ul className={"extras"}>
+                        {extras.map((item,index)=>{
+                            return <li key={index}>{item}</li>;
+                        })}
+                    </ul>
+                </section>
+                <section>
                     <h3>Reservation </h3>
                     <div>
-                        <Calendar />
+                        <Calendar/>
                     </div>
 
                 </section>
-
-
             </>
         );
     }
