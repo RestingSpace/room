b9830c9b2929851207cb9a1e86a24b9f09e7beab
jiuchao <jiuchao1@gmail.com>
2020-05-27 00:25:22 -0400

    update register page

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -47,12 +47,27 @@ class Auth extends Component {
                 {
                     !this.state.isRegister?
                     <div className="login">
+<<<<<<< Updated upstream
                         <input type="text" placeholder="email"
                                onChange={(e) => {
                                    this.setState({email: e.target.value})
                                }}
                         /> <br/><br/>
                         <input type="text" placeholder="password"
+=======
+                        <h2> Login </h2>
+                        {this.state.email.length === 0 ? null : <div className='reminder'>email</div>}
+                        <input className = "login_input" type="text" placeholder=" email"
+                               onChange={(e) => {
+                                   this.setState({email: e.target.value})
+                               }}>
+
+                        
+                        </input>
+                        <br/><br/>
+                        {this.state.password.length === 0 ? null : <div className='reminder'>password</div>}
+                        <input className = "login_input" type="text" placeholder=" password"
+>>>>>>> Stashed changes
                                onChange={(e) => {
                                    this.setState({password: e.target.value})
                                }}
@@ -63,38 +78,92 @@ class Auth extends Component {
                     </div>
                         :
                         <div className="login">
+<<<<<<< Updated upstream
                             <input type="text" placeholder="username"
-                                   onChange={(e) => {
-                                       this.setState({username: e.target.value})
-                                   }}
-                            /> <br/><br/>
-                            <input type="text" placeholder="email"
-                                   onChange={(e) => {
-                                       this.setState({email: e.target.value})
-                                   }}
-                            /> <br/><br/>
-                            <input type="text" placeholder="password"
-                                   onChange={(e) => {
-                                       this.setState({password: e.target.value})
-                                   }}
-                            /> <br/><br/>
-
-                            <input type="text" placeholder="first name"
+=======
+                            <h2>Sign Up</h2>
+                            <h4>Basic Information </h4>
+                            <input className = "login_input" type="text" placeholder=" first name"
                                    onChange={(e) => {
                                        this.setState({first_name: e.target.value})
                                    }}
-                            /> <br/><br/>
-                            <input type="text" placeholder="last name"
+                            />
+                            <input className = "login_input" type="text" placeholder=" last name"
                                    onChange={(e) => {
                                        this.setState({last_name: e.target.value})
                                    }}
                             /> <br/><br/>
-                            <input type="text" placeholder="address"
+                            <input className = "register_input" type="text" placeholder=" username"
+>>>>>>> Stashed changes
                                    onChange={(e) => {
-                                       this.setState({address: e.target.value})
+                                       this.setState({username: e.target.value})
                                    }}
                             /> <br/><br/>
+<<<<<<< Updated upstream
+                            <input type="text" placeholder="email"
+=======
+                            <input className = "register_input" type="text" placeholder=" email"
+>>>>>>> Stashed changes
+                                   onChange={(e) => {
+                                       this.setState({email: e.target.value})
+                                   }}
+                            /> <br/><br/>
+<<<<<<< Updated upstream
+                            <input type="text" placeholder="password"
+=======
+                            <input className = "register_input" type="text" placeholder=" password"
+>>>>>>> Stashed changes
+                                   onChange={(e) => {
+                                       this.setState({password: e.target.value})
+                                   }}
+                            /> <br/><br/>
+<<<<<<< Updated upstream
+
+                            <input type="text" placeholder="first name"
+=======
+                            <input className = "register_input" type="text" placeholder=" confirm password"
+>>>>>>> Stashed changes
+                                   onChange={(e) => {
+                                       this.setState({c_password: e.target.value})
+                                   }}
+                            /> <br/><br/>
+<<<<<<< Updated upstream
+                            <input type="text" placeholder="last name"
+=======
+
+                            <h4>Contact Information</h4>
+                            <input className = "register_input" type="text" placeholder=" street address"
+>>>>>>> Stashed changes
+                                   onChange={(e) => {
+                                       this.setState({street_address: e.target.value})
+                                   }}
+<<<<<<< Updated upstream
+                            /> <br/><br/>
+                            <input type="text" placeholder="address"
+=======
+                            /><br/>
+                            <input className = "address_input" type="text" placeholder=" city"
+>>>>>>> Stashed changes
+                                   onChange={(e) => {
+                                       this.setState({city: e.target.value})
+                                   }}
+<<<<<<< Updated upstream
+                            /> <br/><br/>
                             <input type="text" placeholder="phone number"
+=======
+                            />
+                            <input className = "address_input" type="text" placeholder=" state"
+                                   onChange={(e) => {
+                                       this.setState({state: e.target.value})
+                                   }}
+                            />
+                            <input className = "address_input" type="text" placeholder=" zip"
+                                   onChange={(e) => {
+                                       this.setState({zip: e.target.value})
+                                   }}
+                            /><br/><br/>
+                            <input className = "register_input" type="text" placeholder=" phone number"
+>>>>>>> Stashed changes
                                    onChange={(e) => {
                                        this.setState({phone: e.target.value})
                                    }}
