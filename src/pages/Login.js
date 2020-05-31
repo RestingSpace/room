d379f915f80813a9612eb3f1c704b6788180bfec
Yuchao Zhang <35940382+yc450894425@users.noreply.github.com>
2020-05-30 20:18:04 -0400

    May 30 by Yucaho Zhang.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,99 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import Auth from '../components/Auth'
-const Login = () => {
-    return (
-        <>
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-        <div>
+class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            password: ""
+        };
 
-        </div>
-        <Hero>
-            <Auth isRegister={false}/>
-        </Hero>
-        </>
-        
-    )
+        this.submitClick = this.submitClick.bind(this);
+        this.handleChangeUsername = this.handleUsernameChange.bind(this);
+        this.handleChangePassword = this.handlePasswordChange.bind(this);
+    }
+
+    handleUsernameChange(event) {
+        this.setState({ username: event.currentTarget.value });
+    }
+
+    handlePasswordChange(event) {
+        this.setState({ password: event.currentTarget.value });
+    }
+
+    submitClick(event) {
+        const form = document.getElementById("log-in-form");
+        if (form.checkValidity()) {
+            event.preventDefault();
+
+            const loginURL = "http://localhost:8080/login";
+
+            const requestOptions = {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                // credentials: "include",
+                body: JSON.stringify(this.state),
+                "Access-Control-Allow-Origin": "*"
+            };
+            fetch(loginURL, requestOptions)
+                .then(res => {
+                    if (res.status === 200) { // success Log in
+                        this.props.history.push("/");
+                        console.log(res);
+                        return res;
+                    } else {
+                        const error = new Error(res.error);
+                        throw error;
+                    }
+                })
+                .then(loggedinUser => window.localStorage.setItem("currentUser", JSON.stringify(loggedinUser)))
+                .catch(err => {
+                    console.error(err);
+                    alert("Error logging in please try again");
+                });
+
+
+
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                <div className="intro">
+                    <h5>Log into your account you created before!!</h5>
+                    <p className="subtitle">Sharing your colorful life!!</p>
+                </div>
+                <form className="form" id="log-in-form" onSubmit={this.submitClick}>
+                    <p>
+                        <label>Username:</label>
+                        <input
+                            type="text"
+                            id="username"
+                            placeholder="Enter your username"
+                            value={this.state.username}
+                            onChange={this.handleChangeUsername}
+                        />
+                    </p>
+                    <p>
+                        <label htmlFor="pwd">Password:(6 or more characters)</label>
+                        <input
+                            id="pwd"
+                            placeholder="Enter password"
+                            type="password"
+                            value={this.state.password}
+                            onChange={this.handleChangePassword}
+                        />
+                    </p>
+                    <button type="submit"  value="log in">
+                        Submit
+                    </button>
+                </form>
+            </div>
+        );
+    }
 }
+
 export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,18 +1,106 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import Auth from '../components/Auth'
-const Register = () => {
-    return (
-        <>
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
-        <div>
+class Register extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            password: "",
 
-        </div>
-        <Hero>
-            <Auth isRegister={true}/>
-        </Hero>
-        </>
-        
-    )
+        };
+        this.handleSubmit = this.submitClick.bind(this);
+        this.handleChangeUserid = this.handleUseridChange.bind(this);
+        this.handleChangePassword = this.handlePasswordChange.bind(this);
+
+    }
+
+    handleUseridChange(event) {
+        this.setState({ username: event.currentTarget.value });
+    }
+    handlePasswordChange(event) {
+        this.setState({ password: event.target.value });
+
+    }
+
+    submitClick(event) {
+        const form = document.getElementById("sign-up-form");
+        if (form.checkValidity()) {
+            event.preventDefault();
+            console.log(this.state.username);
+                // sign up
+                const signupURL = "http://localhost:8080/signup";
+                const requestOptions = {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    // credentials: "include",
+                    body: JSON.stringify({
+                        username: this.state.username,
+                        password: this.state.password,
+
+                    }),
+                    "Access-Control-Allow-Origin": "*"
+                };
+                fetch(signupURL, requestOptions)
+                    .then(res => {
+                        if (res.redirected === true && res.url === signupURL) {
+                            this._availableUserid = false;
+                            alert("This userid has been used, please enter a new userID!!");
+                        } else if (res.status == 201) {
+                        } else {
+                            const error = new Error(res.error);
+                            throw error;
+                        }
+                    })
+                    .then(userInfo => {
+                        console.log("Success", userInfo);
+                    });
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="intro">
+                    <h5>Join Us</h5>
+                    <p className="subtitle">Sharing your colorful life!!</p>
+                </div>
+                <form className="form" id="sign-up-form" onSubmit={this.handleSubmit}>
+                    <p>
+                        <label>User ID:</label>
+                        <input
+                            type="text"
+                            className="w3-input w3-border w3-round-large"
+                            id="userid"
+                            placeholder="Enter numbers and letters as your login ID"
+                            value={this.state.username}
+                            onChange={this.handleChangeUserid}
+                            required
+                        />
+                    </p>
+
+                    <p>
+                        <label htmlFor="pwd">Password:</label>
+                        <input
+                            className="w3-input w3-border w3-round-large"
+                            id="pwd"
+                            placeholder="Enter password for this account"
+                            type="password"
+                            value={this.state.password}
+                            onChange={this.handleChangePassword}
+                            required
+                        />
+                    </p>
+
+
+                    <button type="submit" className="w3-button w3-amber" value="sign up">
+                        Sign Up
+                    </button>
+                </form>
+            </div>
+        );
+    }
 }
+
 export default Register;
