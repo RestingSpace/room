b0472ceca00d9fec1ef12df27f15c35b4d128841
jiuchao <jiuchao1@gmail.com>
2020-06-08 00:05:33 -0400

    add an login icon

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {BsPeopleCircle} from 'react-icons/bs'
 import {useContext} from 'react'
 import {RoomContext} from '../context'
 
@@ -10,15 +11,22 @@ export default function Welcome() {
     console.log(username);
     
     return (
+        <>
         
         <div className = "nav-links a welcome">
+
             {!isLogin ?
-                null 
+                null
                 :
-                <p>Welcome {username}</p>
+                <p>
+                    Welcome {username}
+                </p>
+
+
         }
-            
+         <BsPeopleCircle className = "icon-people"/>
         </div>
+        </>
     )
     
 }
