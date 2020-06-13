e68257847e9eeb3bf855c84260d33f7097f0850b
Qianfan <guoqianfan2013@gmail.com>
2020-06-12 23:48:05 -0400

    logo change

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import loadingGif from '../images/gif/loading-arrow.gif'
 export default function Loading() {
     return (
        <div className="loading">
-           <h4>room data loading ...</h4>
+       
            <img src={loadingGif}></img>
        </div> 
         
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Auth from '../components/Auth'
-import QRCodeDisplay from '../components/QRCodeDisplay'
-import PopUpQRCode from '../components/PopUpQRCode'
+
 const Login = () => {
     return (
         <>
             <Hero>
                 <Auth isRegister={1} />
             </Hero>
-            <QRCodeDisplay></QRCodeDisplay>
-            <PopUpQRCode></PopUpQRCode>
+
         </>
 
     )
